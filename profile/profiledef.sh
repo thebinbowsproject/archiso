e9ebcb902a3011cd331e4686ef03e01cf23893cb
zephyrtm <162458898+zephyruszt@users.noreply.github.com>
2026-06-26 10:21:22 +0200

    fixed profiledef

--- profile/profiledef.sh
+++ profile/profiledef.sh
@@ -6,3 +6,11 @@ iso_version="1.0"
 iso_arch="x86_64"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-ia32.grub.esp' 'uefi-x64.grub.esp')
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/usr/local/bin/binbows-start"]="0:0:755"
+  ["/opt/binbows/netstay/netstay"]="0:0:755"
+  ["/opt/binbows/binamp/binamp"]="0:0:755"
+)
