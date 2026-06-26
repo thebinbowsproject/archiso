0b392df792ecb5f234d0474dd730be3fdd794438
zephyrtm <162458898+zephyruszt@users.noreply.github.com>
2026-06-26 09:59:45 +0200

    Updated profiledef.sh

--- profile/profiledef.sh
+++ profile/profiledef.sh
@@ -1,4 +1,8 @@
 iso_name="binbows98"
-iso_label="BINBOWS_$(date +%Y%m)"
-iso_publisher="The Binbows Project"
+iso_label="BINBOWS98"
+iso_publisher="Michaelsoft Publishing"
 iso_application="Binbows 98"
+iso_version="1.0"
+iso_arch="x86_64"
+buildmodes=('iso')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-ia32.grub.esp' 'uefi-x64.grub.esp')
